da0a4e410934ae57984a1e5277af79579bc80e72
Christopher Drew <Christopher_Drew@s5a.com>
2016-10-21 17:34:47 -0400

    File reader; fixed bugs with the chain being created incorrectly.

---
+++ src/SermonatorRita.java
@@ -0,0 +1,14 @@
+import rita.*;
+
+/**
+ * Created by christopherdrew on 10/21/16.
+ */
+public class SermonatorRita {
+
+    public static void main(String[] args) {
+
+        RiString rs = new RiString("This seems impressive, is it not? Nonsense: pgsdfksa.");
+        System.out.println(rs.features());
+    }
+
+}
